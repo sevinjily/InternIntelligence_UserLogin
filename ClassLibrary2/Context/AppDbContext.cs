37c7f902e6f7f58eb9de1d917da7b4f145e0ef42
svncmsva <sevincmusaeva01@gmail.com>
2025-03-09 00:19:52 +0400

    User model,databas

---
+++ ClassLibrary2/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace DataAccess.Context
+{
+  public class AppDbContext:IdentityDbContext<User,Role,Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        //public int MyProperty { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<User>().ToTable("Users");
+
+            builder.Entity<Role>().ToTable("Roles");
+        }
+
+    }
+}
---
+++ User-Login/Program.cs
@@ -0,0 +1,40 @@
+using DataAccess.Context;
+using Entities.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddIdentity<User,Role>()
+      .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
